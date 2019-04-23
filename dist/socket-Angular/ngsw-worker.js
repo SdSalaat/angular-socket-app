183462a652720b63008f5fda10f73a4c6e8b9c1b
sdSalaat <asamadjhangir@gmail.com>
2019-04-23 10:41:21 +0500

    BUILD 12

--- dist/socket-Angular/ngsw-worker.js
+++ dist/socket-Angular/ngsw-worker.js
@@ -1890,12 +1890,12 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
             this.scope.addEventListener('fetch', (event) => this.onFetch(event));
             this.scope.addEventListener('message', (event) => this.onMessage(event));
             this.scope.addEventListener('push', (event) => this.onPush(event));
-          this.scope.addEventListener('notificationclick', (event) => {
-            event.notification.close();
-            if (clients.openWindow && event.notification.data.url) {
-              event.waitUntil(clients.openWindow(event.notification.data.url));
-            }
-          });
+            this.scope.addEventListener('notificationclick', (event) => {
+              event.notification.close();
+              if (clients.openWindow && event.notification.data.url) {
+                event.waitUntil(clients.openWindow(event.notification.data.url));
+              }
+            });
             // The debugger generates debug pages in response to debugging requests.
             this.debugger = new DebugHandler(this, this.adapter);
             // The IdleScheduler will execute idle tasks after a given delay.
